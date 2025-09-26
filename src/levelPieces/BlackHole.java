fcb6b7617c4034ae9dc660214b2211e2429295f7
leslie_cordova <leslie_cordova@mines.edu>
2025-09-26 07:18:32 -0600

    Added BlackHole

--- src/levelPieces/BlackHole.java
+++ src/levelPieces/BlackHole.java
@@ -1,5 +1,32 @@
 package levelPieces;
 
-public class BlackHole {
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
 
+//BlackHole: hazard.
+//does not move.
+//kills player if within 2 spaces.
+public class BlackHole extends GamePiece {
+
+	//Constructor
+	public BlackHole(int location) {
+		super('X', "Black Hole", location);
+	}
+
+	//if player within 2 spaces then KILL
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		int distance = Math.abs(playerLocation - getLocation());
+
+		if (distance > 0 && distance <= 2) {
+			return InteractionResult.KILL;
+		}
+
+		//in case player lands directly on it
+		if (playerLocation == getLocation()) {
+			return InteractionResult.KILL;
+		}
+
+		return InteractionResult.NONE;
+	}
 }
